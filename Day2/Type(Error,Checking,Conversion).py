c079977e75c7ad4d6867be117b4f16cf4bf7b0db
MohdFazalm99 <mohammadfazalm@gmail.com.com>
2021-06-01 16:41:36 +0530

    Type(error, checking & cnversion) done

---
+++ Day2/Type(Error,Checking,Conversion).py
@@ -0,0 +1,13 @@
+
+num_char = len(input("what is your name ? "))
+
+# Example of type error 
+print("Your name has " + num_char + "characters")
+
+# Example of type checking
+print(type(num_char)) 
+
+# Example of type conversion or type casting
+
+new_num_char = str(num_char)
+print("Your name has " + new_num_char + " characters")
