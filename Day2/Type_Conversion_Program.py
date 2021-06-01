44bdbd83ed26a6a30546e6778e6c136b9a0b6562
MohdFazalm99 <mohammadfazalm@gmail.com.com>
2021-06-01 16:58:35 +0530

    Type conversion program is done

---
+++ Day2/Type_Conversion_Program.py
@@ -0,0 +1,10 @@
+from typing import Sequence
+
+
+two_digit_number = input("Enter two digit number :")
+
+first_digit = two_digit_number[0]
+second_digit = two_digit_number[1]
+
+result = int(first_digit) + int(second_digit)
+print("After subscripting and type conversion our result is :" ,result)
