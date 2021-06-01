470d5d16a88b3b3a785fdac44ced58656544362a
MohdFazalm99 <mohammadfazalm@gmail.com.com>
2021-06-01 15:13:12 +0530

    Length of the Input Data has been covered

--- Day1/inputFunction.py
+++ Day1/inputFunction.py
@@ -1,3 +1,6 @@
 # input() will get user input in colsole
 #Then print() will print the word "Hello" and the user input
 print("Hello " + input("What is your name ?"))
+
+# Print your nam's length
+print(len(input("What is your name ? ")))
