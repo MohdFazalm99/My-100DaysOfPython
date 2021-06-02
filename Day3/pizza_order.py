991e60cad80be59b24b4ae6075e8c3f6a2a03ea3
MohdFazalm99 <mohammadfazalm@gmail.com.com>
2021-06-02 16:01:58 +0530

    Pizza_Order program completed

---
+++ Day3/pizza_order.py
@@ -0,0 +1,34 @@
+
+print("Welcome to Python Pizza Deliveries!")
+size = input("What size pizza do you want? S, M, or L ")
+add_pepperoni = input("Do you want pepperoni? Y or N ")
+extra_cheese = input("Do you want extra cheese? Y or N ")
+
+pizza_price = 0
+
+if size == "S":
+  pizza_price = 15
+  
+  if add_pepperoni == "Y":
+    pizza_price += 2
+  if extra_cheese == "Y":
+    pizza_price += 1 
+  print(f"Your final bill is ${pizza_price}")
+
+elif size == "M":
+  pizza_price += 20
+
+  if add_pepperoni == "Y":
+    pizza_price += 2
+  if extra_cheese == "Y":
+    pizza_price +=1
+  print(f"Your final bill is ${pizza_price}")
+
+else:
+  pizza_price = 25
+
+  if add_pepperoni =="Y":
+    pizza_price += 2
+  if extra_cheese == "Y":
+    pizza_price += 1
+  print(f"Your final bill is ${pizza_price}")
