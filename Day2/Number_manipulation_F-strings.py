eacd7059bff763d49d78273c95284e2d21bf0aaa
MohdFazalm99 <mohammadfazalm@gmail.com.com>
2021-06-02 10:40:45 +0530

    Numaber manipulation dine

---
+++ Day2/Number_manipulation_F-strings.py
@@ -0,0 +1,23 @@
+# Rounding Number 
+
+print(round(8/3))
+print(round(8/3,2))
+print(round(2.6666666666,3))
+
+# FLOOR-DIVISION =Python uses // as the floor division operator and % as the modulo operator. 
+# If the numerator is N and the denominator D, then this equation N = D * ( N // D) + (N % D) is always satisfied. Use floor division operator // or the floor() function of the math module to get the floor division of two integers.
+
+print(8//3)
+
+# User scores a point
+
+# score += 11
+# print(score)
+
+# f-string
+
+score = 0
+height = 1.8
+isWinning = True
+
+print(f"your score is {score}, your height is {height}, you are winning is {isWinning}")
