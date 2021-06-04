e3bf2db000479fc47364a8ece4e223ef46ee8992
MohdFazalm99 <mohammadfazalm@gmail.com.com>
2021-06-04 21:28:57 +0530

    Day 4 started, random module is used

---
+++ Day4/my_module.py
@@ -0,0 +1,3 @@
+# In this we are just defining the pi value and we will use it in the random_module program
+
+pi = 3.14159246
---
+++ Day4/random_module.py
@@ -0,0 +1,22 @@
+# the random module in Python,  is used to generate pseudo-random numbers for various probabilistic distributions.
+
+""" modules - Consider a module to be the same as a code library.
+
+A file containing a set of functions you want to include in your application."""
+
+# first we ahve import the random module in our code
+
+import random
+
+import my_module
+
+random_integer = random.randint(1,10)
+print(random_integer)
+
+print(my_module.pi)
+
+# 0.00000000 - 0.999999...
+random_float = random.random()
+print(random_float)
+
+
