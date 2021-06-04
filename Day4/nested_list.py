73795494c093bf4d74472a9ec0c5004737e87379
MohdFazalm99 <mohammadfazalm@gmail.com.com>
2021-06-05 01:12:06 +0530

    Nested list complete

---
+++ Day4/nested_list.py
@@ -0,0 +1,7 @@
+# Nested List - Where a variable can hold two or more list is one single list
+
+fruits = ["strawberries", "Apples", "Grapes", "peaches", "Pears", "Cherries"]
+vegies = ["spinach", "Kale", "Tomatoes", "Celery", "Potatoes"]
+
+dirty_eleven  = [fruits, vegies]
+print(dirty_eleven)
