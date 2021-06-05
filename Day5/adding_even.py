109c6437f8677127e79c4858a12209d25a0e19db
MohdFazalm99 <mohammadfazalm@gmail.com.com>
2021-06-05 18:30:59 +0530

    Adding even number btw 2 & 100 done

--- Day5/adding_even.py
+++ Day5/adding_even.py
@@ -3,3 +3,9 @@ total = 0
 for num in range(2,101,2):
     total += num
 print("Sum of all even number between 2 & 100 is =" ,total)
+
+total2 = 0
+for number in range(1,101):
+    if number % 2 == 0:
+        total2 += number
+print(total2)
