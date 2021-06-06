13cacf6ce1f6c94017f0a22f5298ef5e532f62a9
MohdFazalm99 <mohammadfazalm@gmail.com.com>
2021-06-06 22:13:41 +0530

    Day6 started

---
+++ Day6/Defining_&_Calling_functions.py
@@ -0,0 +1,22 @@
+# FUNCTIONS in Python
+
+"""
+A function is a block of code which only runs when it is called.
+
+You can pass data, known as parameters, into a function.
+
+A function can return data as a result."""
+
+# Creating a Function
+# In Python a function is defined using the def keyword:
+
+# Example
+def my_function():
+  print("Hello ")
+  print("Bye")
+
+# Calling a Function
+# To call a function, use the function name followed by parenthesis:
+
+my_function()
+
