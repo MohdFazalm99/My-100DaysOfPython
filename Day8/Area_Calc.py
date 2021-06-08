b7055dd02add2cf21ef6533915c0dfe1034998b4
MohdFazalm99 <mohammadfazalm@gmail.com.com>
2021-06-08 11:27:41 +0530

    Some changes done

--- Day8/Area_Calc.py
+++ Day8/Area_Calc.py
@@ -1,18 +1,12 @@
-#Write your code below this line 👇
-
-
-
 
 
 import math
 
-#Write your code above this line 👆
-# Define a function called paint_calc() so that the code below works.   
 def paint_calc(height, width, cover):
    area = height*width
    num_of_cans = math.ceil(area/cover)
    print(f"You will need {num_of_cans} cans of paint")
-  # 🚨 Don't change the code below 👇
+
   
 test_h = int(input("Height of wall: "))
 test_w = int(input("Width of wall: "))
