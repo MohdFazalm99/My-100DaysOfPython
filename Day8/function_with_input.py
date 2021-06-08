e71ff4556b1e9f0caad2eb9a363ad758580f2f4b
MohdFazalm99 <mohammadfazalm@gmail.com.com>
2021-06-08 10:46:59 +0530

    Day8 started, function with input

---
+++ Day8/function_with_input.py
@@ -0,0 +1,19 @@
+# Function with Inputs
+"""
+# Syntax
+
+def my_function(something):
+    Do this "something"
+    Then do this "something"
+    Finally do this "something"
+    
+my_function(123 or anything )
+
+NOTE: Something is called parameter & 123(here) is called Argument"""
+
+# Function that allows for input
+def greet_with_name(name):
+    print(f"Hello {name}")
+    print(f"How do you do {name}")
+
+greet_with_name("Fazal")
