5554e10867fc324a245244ab0d5d2cdada0e2e4a
MohdFazalm99 <mohammadfazalm@gmail.com.com>
2021-06-11 01:36:32 +0530

    Doctype complete

---
+++ Day10/documenting_functon_or_docstrings.py
@@ -0,0 +1,10 @@
+# Doctype example
+# """When we define anything in between these 6 incerted commas it will become a documentaion or docstring of that function or keyword or varibale """
+
+
+def naming(name):
+    """This function is used for naming  """
+    name = input("Enter your name ")
+    return name
+
+print(naming("Fazal"))
