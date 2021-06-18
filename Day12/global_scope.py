3ee93e01b2194a298fbec8b4b768221bb0ba6573
MohdFazalm99 <mohammadfazalm@gmail.com.com>
2021-06-18 20:32:19 +0530

    Global-scope

---
+++ Day12/global_scope.py
@@ -0,0 +1,8 @@
+#----------------Global Scope----------------
+""" Global scope is a varibale that you define and you are not plannig  to change it """
+
+PI = 3.14159
+URL = "https://www.google.com"
+TWITTER_HANDLE = "@yu_angela"
+
+# WE can define global cosntant in lowerCase but it is a standard way to define global constant in UpperCase
