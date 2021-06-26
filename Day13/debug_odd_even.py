49823c3b1efadedb07ae7c94c32c78a29871e2e5
MohdFazalm99 <mohammadfazalm@gmail.com.com>
2021-06-26 20:35:50 +0530

    Debugging odd and even done

---
+++ Day13/debug_odd_even.py
@@ -0,0 +1,8 @@
+number = int(input("Which number do you want to check?"))
+
+# We fix the err below where we use ==(equal) in place of =(assign)
+if number % 2 == 0:
+  print("This is an even number.")
+else:
+  print("This is an odd number.")
+  
