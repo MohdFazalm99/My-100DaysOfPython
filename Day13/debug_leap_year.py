1d55d8da35cbdb2cb1adbe974deb99ab9a25fe05
MohdFazalm99 <mohammadfazalm@gmail.com.com>
2021-06-26 20:39:11 +0530

    Debug leap year complete

---
+++ Day13/debug_leap_year.py
@@ -0,0 +1,14 @@
+# here we fix the typeError below where we are asking the year we use int for solving this err
+year = int(input("Which year do you want to check?"))
+
+if year % 4 == 0:
+  if year % 100 == 0:
+    if year % 400 == 0:
+      print("Leap year.")
+    else:
+      print("Not leap year.")
+  else:
+    print("Leap year.")
+else:
+  print("Not leap year.")
+  
